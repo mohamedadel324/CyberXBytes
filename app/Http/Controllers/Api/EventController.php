b149f942158c6e9227e9f7b83b6324cb76b56078
mohamedadel324 <mohamedmersal98@gmail.com>
2025-04-17 23:06:06 +0200

    asdsa

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -26,7 +26,8 @@ class EventController extends Controller
             ->where(function($query) use ($user) {
                 $query->where('registration_start_date', '<=', now())
                     ->orWhereHas('registrations', function($q) use ($user) {
-                        $q->where('user_id', $user->id);
+                        $q->where('user_id', $user->id)
+                          ->where('event_uuid', 'events.uuid');
                     })
                     ->orWhereHas('invitations', function($q) use ($user) {
                         $q->where('email', $user->email);
