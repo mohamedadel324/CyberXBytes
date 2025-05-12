fbd3c38deb383957b7cf6d2f1e99a330b0e42349
mohamedadel324 <mohamedmersal98@gmail.com>
2025-05-13 00:52:21 +0300

    asdasd

---
+++ app/Filament/Resources/AdResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AdResource\Pages;
+use App\Filament\Resources\AdResource\RelationManagers;
+use App\Models\Ad;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AdResource extends Resource
+{
+    protected static ?string $model = Ad::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('image')
+                    ->required()
+                    ->imageEditor()
+                    ->image(),
+                Forms\Components\TextInput::make('link')
+                    ->label('Link (optional)')
+                    ->url(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('link'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAds::route('/'),
+            'create' => Pages\CreateAd::route('/create'),
+            'edit' => Pages\EditAd::route('/{record}/edit'),
+        ];
+    }
+}
