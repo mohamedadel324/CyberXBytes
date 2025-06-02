348b63032dfe670520c6c34e6c84b1f770f5963e
mohamedadel324 <mohamedmersal98@gmail.com>
2025-06-02 16:35:23 +0200

    dasdas

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,11 +5,18 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
-
-class Admin extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+    
+class Admin extends Authenticatable implements FilamentUser
 {
     use HasRoles, Notifiable;
 
+     
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
     protected $guard_name = 'admin';
 
     protected $fillable = [
